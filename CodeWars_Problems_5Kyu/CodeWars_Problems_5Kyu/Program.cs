bf440c88ec44b15f4b535f38e14c283f0eae7aad
Jack Kessler <jack_kessler@hotmail.com>
2023-02-05 12:45:13 -0600

    Save for work so far

--- CodeWars_Problems_5Kyu/CodeWars_Problems_5Kyu/Program.cs
+++ CodeWars_Problems_5Kyu/CodeWars_Problems_5Kyu/Program.cs
@@ -25,21 +25,23 @@ int s2 = 0;
 int t1 = 0;
 int t2 = 0;
 
-int up = 0;
-int dn = 0;
-int right = 0;
-int left = 0;
+int[] xCoordHitTop = { 0 };
+int[] yCoordHitTop = { 0 };
 
-int[] xCoordArray = { 0 };
-int[] yCoordArray = { 0 };
+int[] xCoordHitBottom = { 0 };
+int[] yCoordHitBottom = { 0 };
 
-int tempxCoord = 0;
-int tempyCoord = 0;
+int[] xCoordHitRight = { 0 };
+int[] yCoordHitRight = { 0 };
+
+int[] xCoordHitLeft = { 0 };
+int[] yCoordHitLeft = { 0 };
+
 bool hitExists = false;
 
 string survivors = "";
 int xHitCounter = 0;
-bool resetCounter = true;
+bool breakLoop = false;
 
 for (i = 0; i < image.Length; i++)
 {
@@ -67,25 +69,76 @@ for (i = 0; i < image.Length; i++)
             {
                 image[i][j] = ' ';
             }
-            else if (image[i + 1][j] == '~' || image[i - 1][j] == '~' || image[i][j + 1] == '~' || image[i][j - 1] == '~')
-            //Finding coordinates of hits touching water (up, dn, right, left).
+            else if (image[i + 1][j] == '~')
+            //Finding coordinates of hits touching water on the top (i.e. Submarine case. Water would come down)
             {
-                yCoordArray = yCoordArray.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
+                yCoordHitTop = yCoordHitTop.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
                 
                 //LINQ Append() Method: https://code-maze.com/add-values-to-csharp-array/
 
-                xCoordArray = xCoordArray.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
+                xCoordHitTop = xCoordHitTop.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
                 hitExists = true;
                 xHitCounter++;
-                //code above puts all hit coordinates touching water in two arrarys. One for x coordinates (column) and one for y coordinates (row).
+                //code above puts all hit coordinates touching water above in two arrarys.
+                //One for x coordinates (column) and one for y coordinates (row).
+            }
+            else if (image[i - 1][j] == '~')
+            //Finding coordinates of hits touching water below.
+            {
+                yCoordHitBottom = yCoordHitBottom.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
+
+                //LINQ Append() Method: https://code-maze.com/add-values-to-csharp-array/
+
+                xCoordHitBottom = xCoordHitBottom.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
+                hitExists = true;
+                xHitCounter++;
+                //code above puts all hit coordinates touching water below in two arrarys.
+                //One for x coordinates (column) and one for y coordinates (row).
+            }
+            else if (image[i][j + 1] == '~')
+            //Finding coordinates of hits touching water to the right.
+            {
+                yCoordHitRight = yCoordHitRight.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
+
+                //LINQ Append() Method: https://code-maze.com/add-values-to-csharp-array/
+
+                xCoordHitRight = xCoordHitRight.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
+                hitExists = true;
+                xHitCounter++;
+                //code above puts all hit coordinates touching water to the right in two arrarys.
+                //One for x coordinates (column) and one for y coordinates (row).
+            }
+            else if (image[i][j - 1] == '~')
+            //Finding coordinates of hits touching water to the left.
+            {
+                yCoordHitLeft = yCoordHitLeft.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
+
+                //LINQ Append() Method: https://code-maze.com/add-values-to-csharp-array/
+
+                xCoordHitLeft = xCoordHitLeft.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
+                hitExists = true;
+                xHitCounter++;
+                //code above puts all hit coordinates touching water to the left in two arrarys.
+                //One for x coordinates (column) and one for y coordinates (row).
             }
         }
     }
 
 }
 
-xCoordArray = xCoordArray.Skip(1).ToArray(); //remove initial 0 entry at index 0. Note: last index now becomes 0. 
-yCoordArray = yCoordArray.Skip(1).ToArray(); //remove initial 0 entry at index 0. Note: last index now becomes 0.
+xCoordHitTop = xCoordHitTop.Skip(1).ToArray(); //remove initial 0 entry at index 0. Note: last index now becomes 0. 
+yCoordHitTop = yCoordHitTop.Skip(1).ToArray(); //remove initial 0 entry at index 0. Note: last index now becomes 0.
+
+xCoordHitBottom = xCoordHitBottom.Skip(1).ToArray();
+yCoordHitBottom = yCoordHitBottom.Skip(1).ToArray() ;
+
+xCoordHitRight = xCoordHitRight.Skip(1).ToArray();
+yCoordHitRight = yCoordHitRight.Skip(1).ToArray();
+
+xCoordHitLeft = xCoordHitLeft.Skip(1).ToArray();
+yCoordHitLeft = yCoordHitLeft.Skip(1).ToArray();
+
+
 
 i = 0; //reset counter i to zero for loop below
 j = 0; //reset counter j to zero for loop below
@@ -97,126 +150,163 @@ if (hitExists == false)
 }
 else
 {   
-    while (resetCounter == true)
-    {
-        for (i = 0; i < yCoordArray.Length; i++)
-        // Need hits to stretch vertically where possible. Stretch from bottom to top first.
-        // Code below is for water coming from bottom of the boat. Holes on the side will be taken care of later.
+        for (i = 0; i < yCoordHitTop.Length; i++)
+        // First, stretch hits starting at the top in the downward direction (i.e. case of submarine)
+        // +k because of inversion. remember, bottom of array = largest index
         {
-            if (image[yCoordArray[i] + k][xCoordArray[j]] == ' ' && yCoordArray[i] + k < image.Length)
+            if (image[yCoordHitTop[i] + k][xCoordHitTop[i]] == ' ') //looking at index k spaces beneath original hit
             {
-                image[yCoordArray[i] + k][xCoordArray[j]] = 'x';
-                i = 0;
+                image[yCoordHitTop[i] + k][xCoordHitTop[i]] = 'x';
+                i = -1;
                 k++;
             }
             else
             {
                 k = 1;
-                j++;
             }
         }
-        for (i = 0; i < yCoordArray.Length; i++)
-        // Next, stretch from top to bottom (only in case of a submarine). Holes on the side will be taken care of later.
+
+        for (i = 0; i < yCoordHitBottom.Length; i++)
+        // Next, stretch hits starting at the bottom in the upward direction
         {
-            if (image[yCoordArray[i - k]][xCoordArray[j]] == ' ')
+            if (image[yCoordHitBottom[i] - k][xCoordHitBottom[i]] == ' ') //looking at index k spaces above original hit
             {
-                image[yCoordArray[i + k]][xCoordArray[j]] = 'x';
-                i = 0;
+                image[yCoordHitBottom[i] - k][xCoordHitBottom[i]] = 'x';
+                i = -1;
                 k++;
             }
             else
             {
                 k = 1;
-                j++;
             }
         }
 
-        while (image[yCoordArray[i + 1]][xCoordArray[j]] == ' ' ||
-               image[yCoordArray[i - 1]][xCoordArray[j]] == ' ')
+        for (i = 0; i < xCoordHitLeft.Length; i++)
+        // Next, stretch hits starting at the left to the right
         {
-            if (image[yCoordArray[i + 1]][xCoordArray[j]] == ' ')
+            if (image[yCoordHitLeft[i]][xCoordHitLeft[j] + k] == ' ') //looking at index k spaces to the right of original hit
+        {
+                image[yCoordHitLeft[i]][xCoordHitLeft[j] + k] = 'x';
+                i = -1;
+                k++;
+            }
+            else
             {
-                image[yCoordArray[i + 1]][xCoordArray[j]] = 'x';
-                i++;
+                k = 1;
             }
-            else if (image[yCoordArray[i - 1]][xCoordArray[j]] == ' ')
+        }
+
+        for (i = 0; i < xCoordHitRight.Length; i++)
+        // Next, stretch hits starting at the right to the left
+        {
+            if (image[yCoordHitRight[i]][xCoordHitRight[j] - k] == ' ') //looking at index k spaces to the left of original hit
+        {
+                image[yCoordHitRight[i]][xCoordHitRight[j] - k] = 'x';
+                i = -1;
+                k++;
+            }
+            else
             {
-                image[yCoordArray[i - 1]][xCoordArray[j]] = 'x';
-
+                k = 1;
             }
         }
-    }
-    while (resetCounter == true)
+
+    // To this point, we have identified the coordinates of Frank, Sam, and Tom...
+    // and we have identified at stretched hits that initially touched water.
+    // Now, we have to see if water (coming through the hits) will ever reach Frank, Sam, or Tom.
+
+    //Frank         **Still need to exclude if they run into eachother
+
+    int[] tempFrankY = { f1 };
+    int[] tempFrankX = { f2 };
+    
+
+    //yCoordHitTop = yCoordHitTop.Append(i).ToArray(); //tells us which row we are looking at (y-axis)
+    //CoordHitTop = xCoordHitTop.Append(j).ToArray(); //tells us which column (position within a subarray) we are looking at (x axis)
+
+    image[s1][s2] = ' '; //temporarily making Sam location space. Will fix back later.
+    image[t1][t2] = ' '; //temporarily making Tom location space. Will fix back later.
+
+    k = 1;
+
+    for (i = 0; i < tempFrankX.Length; i++)
     {
-        if (up + 1 <= image.Length && image[up + 1][f2] == 'x' || //making sure array is in-bounds and checking for x
-            dn - 1 >= 0 && image[dn - 1][f2] == 'x' ||
-            right + 1 <= image.Length && image[f1][right + 1] == 'x' ||
-            left - 1 >= 0 && image[f1][left - 1] == 'x')
+        if (tempFrankX[i] + k < image[f1].Length && image[tempFrankY[i]][tempFrankX[i]+k] == ' ') 
+           //making sure array is in-bounds and replacing space to right of Frank with 'F'
         {
-            survivors = "";
-            resetCounter = false;
+            image[tempFrankY[i]][tempFrankX[i] + k] = 'F';
+            tempFrankY = tempFrankY.Append(f1).ToArray(); //recording new Y coordinate that must be re-evaluated for spaces next to
+            tempFrankX = tempFrankX.Append(f2 + k).ToArray(); //recording new X coordinate that must be re-evaluated for spaces next to
+            i = -1;
+            k++;
         }
-        else if (up + 1 <= image.Length && image[up + 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
+        else if (tempFrankX[i] - k >= 0 && image[tempFrankY[i]][tempFrankX[i] - k] == ' ')
+            //making sure array is in-bounds and replacing space to left of Frank with 'F'
         {
-            up = f1 + 1;
+            image[tempFrankY[i]][tempFrankX[i] - k] = 'F';
+            tempFrankY = tempFrankY.Append(f1).ToArray(); //recording new Y coordinate that must be re-evaluated for spaces next to
+            tempFrankX = tempFrankX.Append(f2 - k).ToArray(); //recording new X coordinate that must be re-evaluated for spaces next to
+            i = -1;
+            k++;
         }
-        else if (dn - 1 >= 0 && image[dn - 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
+        else if (tempFrankY[i] + k < image.Length && image[tempFrankY[i] + k][tempFrankX[i]] == ' ')
+            //making sure array is in-bounds and replacing space below Frank with 'F'
         {
-            dn = f1 - 1;
+            image[tempFrankX[i] + 1][tempFrankY[i]] = 'F';
+            tempFrankX = tempFrankX.Append(f1 + k).ToArray(); //recording new X coordinate that must be re-evaluated for spaces next to
+            tempFrankY = tempFrankY.Append(f2).ToArray(); //recording new Y coordinate that must be re-evaluated for spaces next to
+            i = -1;
+            k++;
         }
-        else if (right + 1 <= image.Length && image[f1][right + 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
+        else if (tempFrankY[i] - k < image.Length && image[tempFrankY[i] - k][tempFrankX[i]] == ' ') 
+            //making sure array is in-bounds and replacing space above Frank with 'F'
         {
-            right = f2 + 1;
-        }
-        else if (left - 1 >= 0 && image[f1][left - 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
-        {
-            left = f2 - 1;
-        }
-        else
-        {
-            survivors = survivors + "Frank";
-            resetCounter = false;
+            image[tempFrankX[i] - k][tempFrankY[i]] = 'F';
+            tempFrankX = tempFrankX.Append(f1 - k).ToArray(); //recording new X coordinate that must be re-evaluated for spaces next to
+            tempFrankY = tempFrankY.Append(f2).ToArray(); //recording new Y coordinate that must be re-evaluated for spaces next to
+            i = -1;
+            k++;
         }
     }
 
+    //Add code for if F is next to 'X', then Frank is dead
 
-    //Frank **Still need to exclude if they run into eachother
-    up = f1;
-    dn = f1;
-    right = f2;
-    left = f2;
-    while (resetCounter < 1)
-    {
-        if (up + 1 <= image.Length && image[up + 1][f2] == 'x' || //making sure array is in-bounds and checking for x
-            dn - 1 >= 0 && image[dn - 1][f2] == 'x' ||
-            right + 1 <= image.Length && image[f1][right + 1] == 'x' ||
-            left - 1 >= 0 && image[f1][left - 1] == 'x')
-        {
-            survivors = "";
-            resetCounter = 1;
-        }
-        else if (up + 1 <= image.Length && image[up + 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
-        {
-            up = f1 + 1;
-        }
-        else if (dn - 1 >= 0 && image[dn - 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
-        {
-            dn = f1 - 1;
-        }
-        else if (right + 1 <= image.Length && image[f1][right + 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
-        {
-            right = f2 + 1;
-        }
-        else if (left - 1 >= 0 && image[f1][left - 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
-        {
-            left = f2 - 1;
-        }
-        else
-        {
-            survivors = survivors + "Frank";
-            resetCounter = 1;
-        }
-    }
+    //Now do the same with Sam and Tom
+
+
+    //while (breakLoop == false)
+    //{
+    //    if (up + 1 < image.Length && image[up + 1][f2] == 'x' || // making sure array is in-bounds. If x is next to up, dn, right, or left...
+    //        dn - 1 >= 0 && image[dn - 1][f2] == 'x' ||           // water has reached Frank.
+    //        right + 1 < image[f1].Length && image[f1][right + 1] == 'x' ||
+    //        left - 1 >= 0 && image[f1][left - 1] == 'x')
+    //    {
+    //        survivors = "";
+    //        breakLoop = true;
+    //    }
+    //    else if (up + 1 < image.Length && image[up + 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
+    //    {
+    //        tempFrankX = tempFrankX.Append(up + 1).ToArray();
+    //        up++;
+    //    }
+    //    else if (dn - 1 >= 0 && image[dn - 1][f2] == ' ') //making sure array is in-bounds and replacing space with 'x'
+    //    {
+    //        dn--;
+    //    }
+    //    else if (right + 1 < image[f1].Length && image[f1][right + 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
+    //    {
+    //        right++;
+    //    }
+    //    else if (left - 1 >= 0 && image[f1][left - 1] == ' ') //making sure array is in-bounds and replacing space with 'x'
+    //    {
+    //        left--;
+    //    }
+    //    else
+    //    {
+    //        survivors = survivors + "Frank";
+    //        breakLoop = true;
+    //    }
+    //}
 }
 
 
